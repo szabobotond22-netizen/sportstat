b3309755708bb4cb4da766f7ea6388d3676c77a4
KoviSomaMester <113170219+KoviSomaMester@users.noreply.github.com>
2026-05-03 21:00:44 +0200

    Add Jest configuration file for testing

---
+++ backend/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/tests/**/*.test.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testTimeout: 30000
+};
